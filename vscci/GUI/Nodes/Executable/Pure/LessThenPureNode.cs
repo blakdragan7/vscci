a657376ba745c6e68efbe394ec272102277e02bd
blakdragan7 <jcopela4@Gmail.com>
2021-07-28 23:13:49 -0400

    Added a bunch of basic pure nodes for comparisons

---
+++ vscci/GUI/Nodes/Executable/Pure/LessThenPureNode.cs
@@ -0,0 +1,27 @@
+﻿namespace VSCCI.GUI.Nodes
+{
+    using Cairo;
+    using System;
+    using Vintagestory.API.Client;
+
+    class LessThenPureNode<T> : ExecutableScriptNode where T : IComparable
+    {
+        public LessThenPureNode(ICoreClientAPI api, Matrix nodeTransform, ElementBounds bounds) : base("<", api, nodeTransform, bounds, true)
+        {
+            inputs.Add(new ScriptNodeInput(this, "First", typeof(T)));
+            inputs.Add(new ScriptNodeInput(this, "Second", typeof(T)));
+
+            outputs.Add(new ScriptNodeOutput(this, "Result", 1, typeof(bool)));
+
+            shouldAutoExecuteNext = false;
+        }
+
+        protected override void OnExecute()
+        {
+            dynamic first = inputs[0].GetInput();
+            dynamic second = inputs[1].GetInput();
+
+            outputs[0].Value = first < second;
+        }
+    }
+}
