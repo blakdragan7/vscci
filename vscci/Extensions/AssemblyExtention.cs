c5183228f621e8a51438d4962d877676832fc6a1
blakdragan7 <jcopela4@Gmail.com>
2021-08-05 00:01:21 -0400

    Extension for getting all types even if some fail to load

---
+++ vscci/Extensions/AssemblyExtention.cs
@@ -0,0 +1,23 @@
+﻿namespace VSCCI.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class AssemblyExtention
+    {
+        public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+        {
+            // TODO: Argument validation
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null);
+            }
+        }
+    }
+}
