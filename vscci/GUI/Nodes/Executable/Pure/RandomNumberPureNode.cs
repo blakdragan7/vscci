cd8919fd9c5132cc50cc909dfd9cfcb93bd45855
blakdragan7 <jcopela4@Gmail.com>
2021-08-19 01:24:25 -0400

    Finished Random pure node

---
+++ vscci/GUI/Nodes/Executable/Pure/RandomNumberPureNode.cs
@@ -0,0 +1,38 @@
+﻿namespace VSCCI.GUI.Nodes
+{
+    using Vintagestory.API.Client;
+    using VSCCI.GUI.Elements;
+    using VSCCI.GUI.Nodes.Attributes;
+    using VSCCI.GUI.Pins;
+
+    [NodeData("Util", "Random Number")]
+    [InputPin(typeof(Number), 0)]
+    [InputPin(typeof(Number), 1)]
+    [OutputPin(typeof(Number), 0)]
+    public class RandomNumberPureNode : ExecutableScriptNode
+    {
+        private readonly System.Random random = new System.Random();
+
+        public RandomNumberPureNode(ICoreClientAPI api, MatrixElementBounds bounds) : base("Random", api, bounds, true)
+        {
+            inputs.Add(new ScriptNodeNumberInput(this, "Min"));
+            inputs.Add(new ScriptNodeNumberInput(this, "Max"));
+            outputs.Add(new ScriptNodeOutput(this, "Result", typeof(Number)));
+        }
+
+        protected override void OnExecute()
+        {
+            Number min = inputs[0].GetInput();
+            Number max = inputs[1].GetInput();
+
+            Number result = random.Next(min, max);
+
+            outputs[0].Value = result;
+        }
+
+        public override string GetNodeDescription()
+        {
+            return "Returns a random number between \"Min\" and \"Max\" Inclusive";
+        }
+    }
+}
